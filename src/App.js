d3c15fae9d75860d3956c6d60788a410ed5260f1
Simon Nistad <simon.nistad@skatteetaten.no>
2017-07-04 15:35:49 +0200

    Strippet vekk unødvendige ting

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import './App.css';
 
-const Test = styled.div`
+const AppContainer = styled.div`
     color: pink;
 `;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Test>Hello world</Test>
-      </div>
+      <AppContainer>
+          <h1>Hello world</h1>
+      </AppContainer>
     );
   }
 }
